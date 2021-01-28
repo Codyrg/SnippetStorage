fb2a1cffb4c77d7dd97e5c97039f91c02734781e
Codyrg <crgille2@asu.edu>
2021-01-27 22:00:27 -0700

    Create ProfileTests.cs
    Added class for holding profile tests

---
+++ SnippetStorage.Tests/ProfileTests.cs
@@ -0,0 +1,25 @@
+﻿namespace SnippetStorage.Tests
+{
+    using System;
+
+    /// <summary>
+    /// Tests for basic profile operations
+    /// </summary>
+    public class ProfileTests : IDisposable
+    {
+
+        /// <summary>
+        /// Sets up a test profiles
+        /// </summary>
+        public ProfileTests()
+        {
+            
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            
+        }
+    }
+}
